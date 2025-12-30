c7d7b7acf74706deb305c39b6fcbe1454240d97f
KRISHNA-ai-stack <sharmakrishna1142@gmail.com>
2025-12-30 16:04:48 +0530

    Navbar Complete + 2 File Code done

---
+++ src/Component/Navbar-Component/Navbar-item.jsx
@@ -0,0 +1,17 @@
+import "./Navbar-item.css"
+function NavbarItem() {
+    return (
+        <>
+            <a href="#">BESTECH</a>
+            <ul>
+                <li>ABOUT</li>
+                <li>PRODUCTS</li>
+                <li>CONTACT</li>
+                <li>LOGIN</li>
+            </ul>
+            <button>BUY NOW</button>
+        </>
+    )
+}
+
+export default NavbarItem
---
+++ src/Component/Navbar-Component/Navbar.jsx
@@ -0,0 +1,12 @@
+import "./Navbar.css"
+import NavbarItem from "./Navbar-item.jsx"
+
+function Navbar() {
+    return (
+        <div className="NAVBAR">
+            <NavbarItem />
+        </div>
+    )
+}
+
+export default Navbar
