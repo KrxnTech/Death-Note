344ecfabffc0766c840db32aa40ac6d6172a4013
KRISHNA-ai-stack <sharmakrishna1142@gmail.com>
2025-12-30 16:32:15 +0530

    Added Image of L + Box Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import './App.css'
 import Navbar from "./Component/Navbar-Component/Navbar.jsx"
 import Heading from "./Component/Heading-Component/Heading.jsx"
+import ProductCard from './Component/Products-Box/Product-Card.jsx'
 
 function App() {
 
   return (
     <>
-      <Navbar/>
-      <Heading/>
+      <Navbar />
+      <Heading />
+      <div className="PARENTOP">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
     </>
   )
 }
---
+++ src/Component/Products-Box/Product-Card.jsx
@@ -0,0 +1,13 @@
+import "./Product-Card.css"
+
+function ProductCard() {
+    return (
+        <div className="PARENTOP">
+            <div className="PRODUCTCARD">
+                <img src="https://i.pinimg.com/originals/44/fa/a6/44faa65bc57e20d1c8e4abb3e72c0429.jpg" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default ProductCard
